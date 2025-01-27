8182acf9bbeaaa82c82a255acf9bfab2b26ea626
kunal <kunalkhelkar123@gmail.com>
2025-01-28 01:04:59 +0530

    offers tab and popup add   27/01/2025

--- user/src/Components/Investment/EnquireNowForm.jsx
+++ user/src/Components/Investment/EnquireNowForm.jsx
@@ -41,7 +41,7 @@ const EnquireNowForm = () => {
 
     try {
       const response = await fetch(
-        "http://localhost:4000/api/property/addenquiry",
+        "/api/property/addenquiry",
         {
           method: "POST",
           headers: {

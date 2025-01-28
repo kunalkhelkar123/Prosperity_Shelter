098770566a57f5b40ce7bc201be8fd4ab79d574f
kunal <kunalkhelkar123@gmail.com>
2025-01-28 12:13:28 +0530

    28/01/2025

--- user/src/Components/Commercial/Investment/CommertialApp.jsx
+++ user/src/Components/Commercial/Investment/CommertialApp.jsx
@@ -37,6 +37,29 @@ const CommertialApp = () => {
   }, []);
 
   useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const response = await axios.get(
+          "api/property/residential_properties"
+        );
+
+        // console.log("commercial properties call")
+        // const response2 = await axios.get("api/property/Commercial_properties");
+        // console.log("Data from database commercial", response2.data);
+
+        console.log("Data from commercialy", response.data);
+        // setAllProperties(response.data);
+      } catch (error) {
+        console.error("Error fetching properties:", error);
+      }
+    };
+
+    fetchProperties();
+  }, []);
+
+
+
+  useEffect(() => {
     const lastPostIndex = currentPage * postPerPage;
     const firstPostIndex = lastPostIndex - postPerPage;
     setCurrentOutput(coinData.slice(firstPostIndex, lastPostIndex));
--- user/src/Components/Residential/AppResidential.jsx
+++ user/src/Components/Residential/AppResidential.jsx
@@ -22,9 +22,9 @@ const AppResidential = () => {
           "api/property/residential_properties"
         );
 
-        console.log("commercial properties call")
-        const response2 = await axios.get("api/property/Commercial_properties");
-        console.log("Data from database commercial", response2.data);
+        // console.log("commercial properties call")
+        // const response2 = await axios.get("api/property/Commercial_properties");
+        // console.log("Data from database commercial", response2.data);
 
         console.log("Data from database using proxy", response.data);
         setAllProperties(response.data);

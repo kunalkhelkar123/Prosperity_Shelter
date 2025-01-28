1b9cd30f0f960c9509c138160478fdc6c27583fd
kunal <kunalkhelkar123@gmail.com>
2025-01-28 11:21:45 +0530

    28/01/2025

--- user/vite.config.js
+++ user/vite.config.js
@@ -2,16 +2,16 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-    proxy: {
-      '/api': {
-        // target: 'http://localhost:4000/',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   open: true,
+  //   proxy: {
+  //     '/api': {
+  //       // target: 'http://localhost:4000/',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
   build: {
     outDir: 'build', // Replace with your desired folder name
   },

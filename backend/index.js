3205602b68edeb63e4bf4d59fcf40c8039fe57b9
Kunal Khelkar <132210427+kunalkhelkar123@users.noreply.github.com>
2025-02-20 12:48:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -123,7 +123,7 @@ app.use("/api/otp", verifyToken, otpRoute);
 
 
 app.use(express.json({ limit: '100mb' })); // Increase JSON payload limit
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 
 // Static File Serving
